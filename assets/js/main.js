83877c1a7f25c0c19a4ebf684f628d955fba3480
Jheimys Bassani <jheimysbb@gmail.com>
2023-03-29 13:21:03 -0300

    :pef: mudando as tag das classes card__image e accordian__card para tag section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const questions = document.querySelectorAll('.question')
 questions.forEach((question) => {
     question.addEventListener('click', () => {
         const questionCliked = question.parentNode.classList.contains('active')
-        console.log(questionCliked)
+        //console.log(questionCliked)
 
         if(questionCliked === false) {
             question.parentNode.classList.add('active')
